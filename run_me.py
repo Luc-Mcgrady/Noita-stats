cd069d21cea6c2e4bbc4d8e54d8adaef122ad10f
Luc <lucmcgrady@gmail.com>
2021-06-02 18:46:22 +0100

    Neatened output

--- run_me.py
+++ run_me.py
@@ -1,6 +1,8 @@
 import stat_loader
 import class_sum
+import json
 
 if __name__ == '__main__':
     out_dict = class_sum.sum_classes(stat_loader.load_stats())
-    print(out_dict)
+    print(json.dumps(out_dict, indent=4))
+    input("Press enter to close: ")
