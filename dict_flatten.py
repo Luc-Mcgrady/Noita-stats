99810a2f66e56ff6b20bc45b4f910bce27e097dd
Luc <lucmcgrady@gmail.com>
2021-06-04 00:13:10 +0100

    Simplified: added class for line rather than tuple Simplified: moved flattening to new file

---
+++ dict_flatten.py
@@ -0,0 +1,28 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class LineData:
+    key_path: tuple[str]
+    new_indent: int
+    key: str
+    value: str
+
+
+def get_lines(dct: dict, _key_path=None) -> list[LineData]:
+    # (key_path, new_indent, key, value,)
+
+    if _key_path is None:
+        _key_path = []
+
+    out = []
+    for key, val in dct.items():
+        if isinstance(val, dict):
+            new_out = get_lines(val, _key_path + [key])
+            if len(new_out):
+                out.append(LineData(tuple(), 1, str(key), ''))
+                out += new_out
+                out[-1].new_indent = -1
+        else:
+            out.append(LineData(tuple(_key_path), 0, str(key), str(val)))
+    return out
--- gui.py
+++ gui.py
@@ -1,8 +1,6 @@
 import stat_loader
 import class_sum
-
-import json
-import tkinter as tk
+import dict_flatten
 
 import flask
 
@@ -12,22 +10,10 @@ def make_server(data):
 
     totals = class_sum.sum_classes(data)
 
-    def get_lines(dct: dict, indent=True) -> list[tuple[int, str, str]]:  # (indent, key, value)
-        out = []
-        for key, val in dct.items():
-            if isinstance(val, dict):
-                new_out = get_lines(val, indent)
-                if len(new_out):
-                    out.append((int(indent), str(key), ''))
-                    out += new_out
-                    out[-1] = (int(-indent), *out[-1][1:])
-            else:
-                out.append((0, str(key), str(val)))
-        return out
+    lines = dict_flatten.get_lines(totals)
 
     @app.route('/')
     def main():
-        lines = get_lines(totals)
         return flask.render_template("line_template.html", lines=lines)
 
     return app
