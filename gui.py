f348d9ec8305c98d7aa44e9d9998505703b17be7
Luc <lucmcgrady@gmail.com>
2021-06-04 18:00:29 +0100

    Added: Time Proportional graphing

--- gui.py
+++ gui.py
@@ -6,15 +6,16 @@ import dict_flatten
 import stat_loader
 
 
-def graph_keys(key_path: list[str], data: dict):
-    def get_keys(keys: list[str], dct: dict):
-        to_get = dct
-        for key in keys:
-            if not isinstance(to_get, dict):
-                to_get = to_get.__dict__
-            to_get = to_get.get(key, 0)
-        return to_get
+def get_keys(keys: list[str], dct: dict):
+    to_get = dct
+    for key in keys:
+        if not isinstance(to_get, dict):
+            to_get = to_get.__dict__
+        to_get = to_get.get(key, 0)
+    return to_get
 
+
+def graph_keys(key_path: list[str], data: dict):
     to_plot = [get_keys(key_path, a.__dict__) for a in data.values()]
 
     plt.plot([str(a) for a in range(len(to_plot))], to_plot)
@@ -40,6 +41,21 @@ def make_server(data):
         plt.close()
         graph_keys(key_path_array, data)
         plt.title(f"Graph of \"{key_path.replace('_', ' ')}\" per run")
+
+        plt.show()
+
+        return '', 204
+
+    @app.route('/graph_time/<path:key_path>')
+    def graph_over_time(key_path: str):
+        key_path_array = key_path.split('/')
+        to_plot = [
+            get_keys(key_path_array, a.__dict__) / get_keys(["stats", "playtime"], a.__dict__)
+            for a in data.values()]
+
+        plt.close()
+        plt.plot([str(a) for a in range(len(to_plot))], to_plot)
+        plt.title(f"Graph of\n\"{key_path.replace('_', ' ')}\"\ndivided by playtime per run")
 
         plt.show()
 
